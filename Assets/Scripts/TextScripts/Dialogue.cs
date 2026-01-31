83113d1340aad7f463c0f5980de9b2bba3302ddc
Liam York <yorkliam08@gmail.com>
2026-01-30 20:55:05 -0600

    imported Dialog Scripts

---
+++ Assets/Scripts/TextScripts/Dialogue.cs
@@ -0,0 +1,221 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+  
+
+    public DialogGraph lines;
+
+
+    public DialogSegment activeSegment;
+    public float textSpeed;
+
+    private int index;
+
+
+    //public PlayerController pc;
+
+    public bool textActive = false;
+    public UnityEvent EndDialogueEvent;
+
+     public string[] dialogText;
+
+
+    void Start()
+    {
+        if (EndDialogueEvent == null)
+        {
+            EndDialogueEvent = new UnityEvent();
+        }
+          
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && textActive == true)
+        {
+            LineSkip();
+        }
+
+    }
+
+    public void LineSkip()
+    {
+        
+        if ( TextBoxManager.Instance.textComponent.text == activeSegment.DialogText[index])
+        {
+             if (index < activeSegment.DialogText.Length - 1)
+             {
+                NextLine();
+             }
+            else
+            {
+                NextNode();
+            }
+        }
+        else
+        {
+            StopAllCoroutines();
+             TextBoxManager.Instance.textComponent.text = activeSegment.DialogText[index];
+        }
+    }
+    
+     
+
+    public void StartDiolague()
+    {
+        Debug.Log("Start Text");
+         foreach (DialogSegment node in lines.nodes)
+            {
+                if (!node.GetInputPort("input").IsConnected)
+                {
+                    UpdateDialog(node);
+                }
+            }
+         TextBoxManager.Instance.nameText.text = activeSegment.speakerName;
+         TextBoxManager.Instance.portrait.sprite = activeSegment.portrait;
+        textActive = true;
+        //pc = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        TextBoxManager.Instance.textBox.Play("TextBoxAnimation");
+         TextBoxManager.Instance.topBoxAnim.Play("TopBox");
+         TextBoxManager.Instance.textComponent.text = string.Empty;
+        index = 0;
+        //pc.inText = true;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in activeSegment.DialogText[index].ToCharArray())
+        {
+             TextBoxManager.Instance.textComponent.text += c;
+            yield return new WaitForSecondsRealtime(textSpeed);
+        }
+
+    }
+
+    public void NextLine()
+    {
+       
+        
+            index++;
+             TextBoxManager.Instance.textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        
+      
+    }
+
+    public void NextNode()
+    {
+        if(!(activeSegment is DialogAnswerSegments)){
+        
+            if (activeSegment.GetPort("output").IsConnected)
+            {
+                UpdateDialog(activeSegment.GetPort("output").Connection.node as DialogSegment);
+                  TextBoxManager.Instance.textComponent.text = string.Empty;
+                 StartCoroutine(TypeLine());
+            }
+            else
+            {
+                EndDialogue();
+            }
+        }
+        else
+        {
+            if((activeSegment as DialogAnswerSegments).Answers.Count > 0)
+            {
+                 int answerIndex = 0;
+                  foreach (Transform child in  TextBoxManager.Instance.buttonParent)
+                    {
+                        Destroy(child.gameObject);
+                    }
+                 
+                 foreach(string answer in (activeSegment as DialogAnswerSegments).Answers)
+                {
+                   
+                    GameObject btn = Instantiate( TextBoxManager.Instance.buttonPrefab,  TextBoxManager.Instance.buttonParent);
+                    btn.GetComponentInChildren<TMP_Text>().text = answer;
+
+                    int index = answerIndex;
+
+                     btn.GetComponentInChildren<Button>().onClick.AddListener((() => { AnswerClicked(index); }));
+
+                    answerIndex++;
+                }
+            }
+            else
+            {
+                if (activeSegment.GetPort("output").IsConnected)
+                {
+                    UpdateDialog(activeSegment.GetPort("output").Connection.node as DialogSegment);
+                     TextBoxManager.Instance.textComponent.text = string.Empty;
+                    StartCoroutine(TypeLine());
+                }
+                else
+                {
+                    EndDialogue();
+                }
+            }
+        }
+           
+        
+    }
+
+     public void AnswerClicked(int clickedIndex)
+    {
+        XNode.NodePort port = activeSegment.GetPort("Answers " + clickedIndex);
+        if (port.IsConnected)
+        {
+            UpdateDialog(port.Connection.node as DialogSegment);
+            LineSkip();
+        }
+
+        else
+        {
+            EndDialogue();
+        }
+            
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End here");
+       
+         foreach (Transform child in  TextBoxManager.Instance.buttonParent)
+        {
+            Destroy(child.gameObject);
+        }
+        textActive = false;
+        //pc.inText = false;
+        TextBoxManager.Instance.textBox.Play("CloseBox");
+        TextBoxManager.Instance.topBoxAnim.Play("CloseTopBox");
+        //gameObject.SetActive(false);
+        TextBoxManager.Instance.skip.SetActive(false);
+        TextBoxManager.Instance.nameTextObj.SetActive(false);
+        Time.timeScale = 1.0f;
+         TextBoxManager.Instance.textComponent.text = string.Empty;
+        //topBox.SetActive(false);
+        EndDialogueEvent.Invoke();
+        
+    }
+
+     private void UpdateDialog(DialogSegment newSegment)
+        {
+            index = 0;
+            activeSegment = newSegment;
+            dialogText = newSegment.DialogText;
+             TextBoxManager.Instance.nameText.text = activeSegment.speakerName;
+             TextBoxManager.Instance.portrait.sprite = activeSegment.portrait;
+             foreach (Transform child in  TextBoxManager.Instance.buttonParent)
+            {
+                Destroy(child.gameObject);
+            }
+
+        }
+
+}
---
+++ Assets/Scripts/TextScripts/DialogueInteraction.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueInteraction : MonoBehaviour
+{
+     public bool InRange = false;
+
+
+
+  
+
+     public Dialogue text;
+
+     public bool pauseGame = false;
+
+     public bool once = false;
+
+    public bool notAgain = false;
+
+    
+
+    public bool scriptedText;
+
+    public bool canTalk = true;
+
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        TextBoxManager.Instance.DiologueBox.SetActive(false);
+        TextBoxManager.Instance.topBox.SetActive(false);
+        TextBoxManager.Instance.skip.SetActive(false);
+        TextBoxManager.Instance.nameTextObj.SetActive(false);
+    }
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(text.textActive == true)
+        {
+            canTalk = false;
+        }
+         if(InRange == true && canTalk == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Space)&& text.textActive == false && scriptedText == false)
+               {
+                    
+                    TextBoxManager.Instance.DiologueBox.SetActive(true);
+                     TextBoxManager.Instance.topBox.SetActive(true);
+                     TextBoxManager.Instance.skip.SetActive(true);
+                    TextBoxManager.Instance.nameTextObj.SetActive(true);
+                    text.StartDiolague();
+                    
+
+                }
+            }
+        if(text.textActive == false)
+        {
+            canTalk = true;
+        }
+        
+
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+
+        if (other.gameObject.CompareTag("Player")&& text.textActive == false )
+        {
+            InRange = true;
+        }
+        
+        if (other.gameObject.CompareTag("Player") && scriptedText == true)
+        {
+            TextBoxManager.Instance.DiologueBox.SetActive(true);
+             TextBoxManager.Instance.topBox.SetActive(true);
+             TextBoxManager.Instance.skip.SetActive(true);
+            TextBoxManager.Instance.nameTextObj.SetActive(true);
+            text.StartDiolague();
+        }
+
+
+        if (other.gameObject.CompareTag("Player")&& pauseGame == true && once == false){
+             TextBoxManager.Instance.DiologueBox.SetActive(true);
+           
+            text.StartDiolague();
+            Debug.Log("InText");
+            Time.timeScale = 0.0f;
+             
+            once = true;
+
+        }
+
+        if(other.gameObject.CompareTag("Player")&& notAgain == true && once == false)
+        {
+            TextBoxManager.Instance.DiologueBox.SetActive(true);
+            text.StartDiolague();
+            Debug.Log("InText");
+            
+            once = true;
+        }
+
+        if(other.gameObject.CompareTag("Player")&& notAgain == true && once == false &&  notAgain == false)
+        {
+            TextBoxManager.Instance.DiologueBox.SetActive(true);
+            text.StartDiolague();
+            Debug.Log("InText");
+            
+            once = true;
+        }
+
+       
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            InRange = false;
+        }
+    }
+
+    public void StartDialogue()
+    {
+        TextBoxManager.Instance.DiologueBox.SetActive(true);
+        TextBoxManager.Instance.topBox.SetActive(true);
+        TextBoxManager.Instance.skip.SetActive(true);
+        TextBoxManager.Instance.nameTextObj.SetActive(true);
+        text.StartDiolague();
+    }
+
+
+    
+}
