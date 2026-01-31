39d991808b9436b69e91af83d080d0f9738d1d75
Liam York <yorkliam08@gmail.com>
2026-01-30 20:32:48 -0600

    Added Dialog Graph

---
+++ Assets/Scripts/TextScripts/DialogSegment.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System;
+using XNode;
+using System.Collections.Generic;
+using TMPro;
+
+
+[Serializable]
+public struct Connection {}
+public class DialogSegment : Node
+{
+    public Sprite portrait;
+
+    public string speakerName;
+    
+    [Input]
+    public Connection input;
+
+    public string[] DialogText;
+
+     public float textSpeed;
+
+    [Output]
+
+    public Connection output;
+
+    public override object GetValue(NodePort port)
+    {
+        return null;
+    }
+
+
+    
+}
